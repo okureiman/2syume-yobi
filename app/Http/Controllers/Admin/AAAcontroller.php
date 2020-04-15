5d0717d60626351ed6c20b374098ef8eb12787f5
okureiman <ranyakumo7@gmail.com>
2020-04-15 06:18:32 +0000

    first commit

---
+++ app/Http/Controllers/Admin/AAAcontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AAAcontroller extends Controller
+{
+    public function BBB()
+    {
+        return redirect('admin.profile.create');
+    }
+}
